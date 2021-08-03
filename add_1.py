c03ef27dd050c6ae863292b55abc655946ca741c
naoessf <82585072+naoessf@users.noreply.github.com>
2021-08-03 18:14:28 +0900

    Add files via upload

--- add_1.py
+++ add_1.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import operator
 import math as m
+import positionchange() from ex
 
 def gpiosetup():
     GPIO.setmode(GPIO.BOARD)
@@ -31,34 +32,23 @@ def straight():
     straight = SVmotor.ChangeDutyCycle(7.5)
     return straight
 
-departure_lon1 = input("첫 번째 도착지의 경도 : ")
-departure_lat1 = input("첫 번째 도착지의 위도 : ")
-
-departure_lon2 = input("두 번째 도착지의 경도 : ")
-departure_lat2 = input("두 번째 도착지의 위도 : ")
-
-departure_lon3 = input("세 번째 도착지의 경도 : ")
-departure_lat3 = input("세 번째 도착지의 위도 : ")
-
-
-departure_lon = departure_lon1
-deparutre_lat = departure_lat1
-
-
 def direction():
-    #도착지 기준 배의 위치가 1사분면에 있을 경우
-    elif position[3] > longitude(ps) and position[1] > latitude(ps) :
+        # The position of the ship in the 1st quadrant
+    if position[3] > longitude(ps) and position[1] > latitude(ps) :
         difference = (270 - m.atan((position[1] - latitude(ps))/(position[3] - longitude(ps)))) - position[3]
         direction = difference*ME
-        #도착지 기준 배의 위치가 2사분면에 있을 경우
-    if (position[1]) > (latitude(ps)) and (position[2]) < (longitude(ps)) :
+
+    # The position of the ship in the 2nd quadrant
+    elif (position[1]) > (latitude(ps)) and (position[2]) < (longitude(ps)) :
         difference = (90 + m.atan(position[1] - latitude(ps))/(longitude(ps) - position[3])) - position[3]
         direction = difference*ME
-    #도착지 기준 배의 위치가 3사분면에 있을 경우
-    elif: position[1] < latitude(ps) and position[2] < longitude(ps) :
+
+    # The position of the ship in the 3rd quadrant
+    elif position[1] < latitude(ps) and position[2] < longitude(ps) :
         difference = (90 - m.atan((latitude(ps) - position[1])/(longitude(ps) - position[2]))) - position[3]
         direction = differenc*ME
-    #도착지 기준 배의 위치가 4사분면에 있을 경우
+
+    # The position of the ship in the 4th quadrant
     else  :
         difference = (270 + m.atan((latitude(ps) - position[1])/(position[1] - longitude(ps)))) - position[3]
         direction = difference*ME
@@ -66,8 +56,6 @@ def direction():
 # DCmotor
 
 def forward():
-
-    
     forward = DCmotor.ChangeDutyCycle(8)
     return forward
 
@@ -83,12 +71,3 @@ def cleanup():
     SVmotor.stop()
     DCmotor.stop()
     GPIO.cleanup
-
-departure_lon2 = float(input("두 번째 도착지의 위도 : "))
-departure_lat2 = float(input("두 번째 도착지의 위도 : "))
-
-if (departure_lon - 0.08064516) < longitude < (departure_lon + 0.08064516) and (deaparture_lat - 0.08064516) < latitude < (departure_lat - 0.08064516) :
-    
-    departure_lon = departure_lon2
-
-    departure_lat = departure_lat2
